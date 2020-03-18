0a81a2ba8fbc088f07ea607d46c8fdf92f74629d
Taulant Ibraj <tibraj94@gmail.com>
2020-03-18 23:15:40 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(arr1)
+  puts " #{arr1[0]} | #{arr1[1]} | #{arr1[2]} "
+  puts "-----------"
+  puts " #{arr1[3]} | #{arr1[4]} | #{arr1[5]} "
+  puts "-----------"
+  puts " #{arr1[6]} | #{arr1[7]} | #{arr1[8]} "
+end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
